75a3db1a8941a12f2863a08c25622a564692c945
JeffJacobson <JeffJacobson@users.noreply.github.com>
2016-01-14 16:52:30 -0800

    Added UI to index.html
    Added a UI that the user can use to specify an XML document.

--- index.js
+++ index.js
@@ -26,8 +26,18 @@
             window.scroll(0, 0); // scroll back to the top of the page.
             // Stop the navigation.
             return false;
-        } 
+        }
 
+    }
+
+    function disableBootstrapStylesheets() {
+        var i, l, ss, toDisable = /bootstrap/;
+        for (i = 0, l = document.styleSheets.length; i < l; i++) {
+            ss = document.styleSheets[i];
+            if (toDisable.test(ss.href)) {
+                ss.disabled = true;
+            }
+        }
     }
 
     function handleXml(xml) {
@@ -53,23 +63,77 @@
                 link.onclick = handleDataUrlLinkClick;
             }
         });
+
+        disableBootstrapStylesheets();
     }
 
 
 
     if (location.search) {
-        url = location.search.replace(/^\?/, "");
+        url = location.search.match(/ur[li]=([^&]+)/i); //location.search.replace(/^\?/, "");
+    }
+
+    if (url) {
+
+        url = decodeURIComponent(url[1]);
 
         request = new XMLHttpRequest();
         request.open("get", url);
 
+        document.body.classList.add("loading");
+
         request.onloadend = function (e) {
-            xml = (e.target || e.currentTarget || e.originalTarget).responseXML;
-            history.replaceState(e.target.responseText, null, location.href);
-            handleXml(xml);
+            var target = e.target || e.currentTarget || e.originalTarget;
+            document.body.classList.remove("loading");
+            if (target.status === 200) {
+                document.body.innerHTML = "";
+                document.body.classList.add("loaded");
+                xml = target.responseXML;
+                history.replaceState(e.target.responseText, null, location.href);
+                handleXml(xml);
+            }
         };
 
         request.send();
+    } else {
+        // Add bootstrap stylesheets
+        (function () {
+            var template = document.getElementById("bootstrapStylesheetsTemplate");
+            document.head.appendChild(template.content);
+        }());
+    }
+
+    /**
+     * Opens a local XML file and formats it into HTML.
+     * @param {File} file
+     */
+    function openFile(file) {
+        var reader = new FileReader();
+        reader.onloadend = function (progressEvent) {
+            var xml = this.result;
+            document.body.innertHTML = "";
+            document.body.classList.add("loaded");
+            handleXml(xml);
+            history.pushState(xml, null, "#localfile");
+        };
+        reader.readAsText(file);
+    }
+
+    if (document.forms.length > 0) {
+        var form = document.forms[0];
+
+        form.onsubmit = function () {
+            var fileInput = form.querySelector("#fileInput");
+            if (!form.url.value && !fileInput.value) {
+                alert("No XML file specified.");
+                return false;
+            } else if (fileInput.files.length > 0) {
+                openFile(fileInput.files[0]);
+                return false;
+            }
+            // If none of the above conditions are true,
+            // The page will reload with the URL parameter.
+        };
     }
 
     /**
@@ -78,9 +142,13 @@
      * @param {Object} popStateEvent.state
      */
     window.onpopstate = function (popStateEvent) {
-        console.log("pop state", popStateEvent.state);
-        document.body.innerHTML = "";
-        handleXml(popStateEvent.state);
-        window.scroll(0, 0); // scroll back to the top of the page.
+        if (popStateEvent.state) {
+            document.body.innerHTML = "";
+            handleXml(popStateEvent.state);
+            window.scroll(0, 0); // scroll back to the top of the page.
+        } else {
+            // Reload the page if the state has no XML.
+            window.open(window.location, "_self");
+        }
     }
 }());
